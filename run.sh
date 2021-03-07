1ead0b7f1ed24a5b485f689e026004d1a6e982d6
Borhan <borhan.u.cse@gmail.com>
2021-03-07 13:36:27 +0600

    update

--- run.sh
+++ run.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-cd /root/bbb-mp4-old
-node bbb-mp4.js 12345
+file_name=test_files.txt
+ 
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+echo "Current Time : $current_time"
+ 
+new_fileName=$file_name.$current_time
+echo "New FileName: " "$new_fileName"
+ 
+cp $file_name $new_fileName
+echo "You should see new file generated with timestamp on it.."
+
+
+#cd /root/bbb-mp4-old
+#node bbb-mp4.js 12345
 # touch /home/ubuntu/abc.txt
 # echo -e "Hello\nworld"
 # echo -e 'Hello\nworld'
